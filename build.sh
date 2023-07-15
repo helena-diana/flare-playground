f806a231b5f302c11882b6a7405e75e82d7fb037
helena-diana <139609707+helena-diana@users.noreply.github.com>
2023-07-15 06:22:54 +0000

    adding spago purns and purs-tidy as opt deps (using 'npm i' in codespaces terminal sets the enviroment)

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 OUT=docs/app.js
-spago bundle-app --main Main --to $OUT
-uglifyjs $OUT --compress 'pure_getters,keep_fargs=false,unsafe_comps,unsafe' --mangle --output=$OUT
-
+npx spago bundle-app --main Main --to $OUT
+npx uglifyjs $OUT --compress 'pure_getters,keep_fargs=false,unsafe_comps,unsafe' --mangle --output=$OUT
